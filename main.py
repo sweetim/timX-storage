70be16a9a77b18b79c7ae80606415daa42a0a1d2
sweetim <ho@zmp.co.jp>
2016-04-11 09:17:42 +0900

    change debug flag using environment variable

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import webapp2
-
+import os
 
 from controller.api import image
 
 
+debug = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
+
 app = webapp2.WSGIApplication([
     ('/api/image', image.ImageHandler),
     webapp2.Route('/api/image/<image_id>', handler=image.ImageIDHandler)
-], debug=True)
+], debug=debug)
